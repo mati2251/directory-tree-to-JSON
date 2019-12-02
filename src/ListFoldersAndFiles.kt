5e93ff30e035249d26d6615e13eb7b0fb249df6d
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-12-02 22:10:58 +0100

    to json without style

--- src/ListFoldersAndFiles.kt
+++ src/ListFoldersAndFiles.kt
@@ -1,17 +1,17 @@
 import java.io.File
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "NAME_SHADOWING")
-class ListFoldersAndFiles (var folder: File){
+class ListFoldersAndFiles(var folder: File) {
 
     var howManyTabs = 0
+    var file = File("test.json")
 
-    init{
-        if(!folder.exists()){
+    init {
+        if (!folder.exists()) {
             println("Error: Folder dose not exist")
             println("Set current path")
             folder = File(System.getProperty("user.dir"))
-        }
-        else if(folder.isFile){
+        } else if (folder.isFile) {
             println("Error: Path is file")
             println("Set current path")
             folder = File(System.getProperty("user.dir"))
@@ -20,20 +20,48 @@ class ListFoldersAndFiles (var folder: File){
 
     constructor() : this(File(System.getProperty("user.dir"))) {}
 
-    fun checkFolder(){
+    fun checkFolder() {
+        file.printWriter().use { out -> out.println("{") }
         checkFolder(folder)
+        file.appendText("}")
+    }
+
+    private fun addTabsToFile(howManyTabs: Int) {
+        for (i in 0..howManyTabs) {
+            file.appendText("  ")
+        }
     }
 
     private fun checkFolder(folder: File) {
         if (folder.isDirectory) {
             for (i in 0 until folder.listFiles().size) {
-                for(i in 0..this.howManyTabs){
-                    print("     ")
+                addTabsToFile(this.howManyTabs)
+                file.appendText("\"${folder.listFiles()[i].name}\":{ \n")
+                addTabsToFile(this.howManyTabs + 1)
+                file.appendText("\"type\": ")
+                if (folder.listFiles()[i].isDirectory)
+                    file.appendText("\"folder\", \n")
+                else
+                    file.appendText("\"file\", \n")
+                addTabsToFile(this.howManyTabs + 1)
+                file.appendText("\"files\" : {")
+                if (folder.listFiles()[i].isDirectory) {
+                    if (folder.listFiles()[i].listFiles().isEmpty()) {
+                        print("WOW")
+                        file.appendText("\n")
+                    }
                 }
-                println(folder.listFiles()[i].name)
                 this.howManyTabs++
                 checkFolder(File(folder.listFiles()[i].path))
                 this.howManyTabs--
+                file.appendText("} \n")
+                addTabsToFile(this.howManyTabs)
+                file.appendText("}")
+                if (folder.listFiles().size - 1 != i) {
+                    file.appendText(",")
+                }
+                file.appendText("\n")
+
             }
         }
     }
