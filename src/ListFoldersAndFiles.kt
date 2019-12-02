30de9d05da0a4ea8e233e2cd481b25195679e4e1
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-12-02 22:28:34 +0100

    Good style format json

--- src/ListFoldersAndFiles.kt
+++ src/ListFoldersAndFiles.kt
@@ -1,10 +1,10 @@
 import java.io.File
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "NAME_SHADOWING")
-class ListFoldersAndFiles(var folder: File) {
+class ListFoldersAndFiles(var folder: File, fileName: String) {
 
     var howManyTabs = 0
-    var file = File("test.json")
+    var file = File("$fileName.json")
 
     init {
         if (!folder.exists()) {
@@ -18,7 +18,7 @@ class ListFoldersAndFiles(var folder: File) {
         }
     }
 
-    constructor() : this(File(System.getProperty("user.dir"))) {}
+    constructor() : this(File(System.getProperty("user.dir")), "out") {}
 
     fun checkFolder() {
         file.printWriter().use { out -> out.println("{") }
@@ -36,7 +36,7 @@ class ListFoldersAndFiles(var folder: File) {
         if (folder.isDirectory) {
             for (i in 0 until folder.listFiles().size) {
                 addTabsToFile(this.howManyTabs)
-                file.appendText("\"${folder.listFiles()[i].name}\":{ \n")
+                file.appendText("\"${folder.listFiles()[i].name}\": { \n")
                 addTabsToFile(this.howManyTabs + 1)
                 file.appendText("\"type\": ")
                 if (folder.listFiles()[i].isDirectory)
@@ -44,16 +44,22 @@ class ListFoldersAndFiles(var folder: File) {
                 else
                     file.appendText("\"file\", \n")
                 addTabsToFile(this.howManyTabs + 1)
-                file.appendText("\"files\" : {")
+                file.appendText("\"files\": {")
                 if (folder.listFiles()[i].isDirectory) {
-                    if (folder.listFiles()[i].listFiles().isEmpty()) {
-                        print("WOW")
+                    if (folder.listFiles()[i].listFiles().isNotEmpty()) {
                         file.appendText("\n")
+                        howManyTabs++
                     }
                 }
                 this.howManyTabs++
                 checkFolder(File(folder.listFiles()[i].path))
                 this.howManyTabs--
+                if (folder.listFiles()[i].isDirectory) {
+                    if (folder.listFiles()[i].listFiles().isNotEmpty()) {
+                        howManyTabs--
+                        addTabsToFile(this.howManyTabs+1)
+                    }
+                }
                 file.appendText("} \n")
                 addTabsToFile(this.howManyTabs)
                 file.appendText("}")
@@ -61,7 +67,6 @@ class ListFoldersAndFiles(var folder: File) {
                     file.appendText(",")
                 }
                 file.appendText("\n")
-
             }
         }
     }
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,5 @@
 @file:Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 
-import java.io.File
-
 fun main(args: Array<String>) {
     val listFoldersAndFiles = ListFoldersAndFiles()
     listFoldersAndFiles.checkFolder()
